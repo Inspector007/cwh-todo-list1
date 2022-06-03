da23fd07c9ed09a2b11338737238f2d9be7bd8a0
Deepak Singh <deepak.s.singh@globallogic.com>
2022-06-03 12:37:53 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoComponent } from './MyComponent/todo/todo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cwh-todo-list1';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Changed the title";
+    // }, 5000);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './MyComponent/todo/todo.component';
+import { TodoItemComponent } from './MyComponent/todo-item/todo-item.component';
+import { TodoAddComponent } from './MyComponent/todo-add/todo-add.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponent/about/about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoItemComponent,
+    TodoAddComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent},
+      {path: '', component: TodoComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
